2ecc052baea084d36ee41a54c5c541666e22accd
rohandevtown <rohankinnal@devtown.in>
2024-04-07 19:16:21 +0530

    done

--- 1122321231012420124-10/src/pages/Home.Page.jsx
+++ 1122321231012420124-10/src/pages/Home.Page.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Layout HOC
 import DefaultLayoutHoc from '../layout/Default.Layout'
@@ -8,6 +8,7 @@ import EntertainmentCardSlider from '../components/Entertainment/EntertainmentCa
 import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component'
 import PosterSlider from '../components/PosterSlider/PosterSlider.Component'
 
+import axios from 'axios';
 
 const HomePage = () => {
    const [recommendedMovies, setRecommendedMovies] = useState([]);
@@ -15,9 +16,78 @@ const HomePage = () => {
    const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
 
 
+ useEffect(() => {
+    const requestTopRatedMovies = async () => {
+      const getTopRatedMovies = await axios.get(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=b0380404025ff71ff7222783b12710df"
+      );
+      setRecommendedMovies(getTopRatedMovies.data.results);
+    };
+    requestTopRatedMovies();
+  }, []);
+
+  useEffect(() => {
+    const requestUpcomingMovies = async () => {
+      const getUpcomingMovies = await axios.get(
+        "https://api.themoviedb.org/3/movie/upcoming?api_key=b0380404025ff71ff7222783b12710df"
+      );
+      setPMovies(getUpcomingMovies.data.results);
+    };
+    requestUpcomingMovies();
+  }, []);
+
+
+  useEffect(() => {
+    const requestOnlineStreamMoviesMovies = async () => {
+      const getOnlineStreamMovies = await axios.get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=b0380404025ff71ff7222783b12710df"
+      );
+      setOnlineStreamEvents(getOnlineStreamMovies.data.results);
+    };
+    requestOnlineStreamMoviesMovies();
+  }, []);
+
+
   return (
     <>
         <HeroCarousel />
+        <div className='container mx-auto px-4 md:px-12 my-8'>
+            <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 my-3'>The Best of Entertainment</h1>
+            <EntertainmentCardSlider />
+        </div>
+
+        <div className='container mx-auto px-4 md:px-12 my-8'>
+            <PosterSlider 
+            title = "Recommended Movies"
+            subtitle = "List of Recommended Movies"
+            posters= {recommendedMovies}
+            isDark = {false}
+            />
+        </div>
+
+        <div className='bg-premier-800 py-12'>
+            <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+                <div className='hidden md:flex'>
+                    <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="RuPay"  className='w-full h-full'
+                    />
+                </div>
+                 <PosterSlider 
+            title = "Premiers"
+            subtitle = "Brand new release every Friday"
+            posters= {premierMovies}
+            isDark = {true}
+            />
+            </div>
+        </div>
+
+        <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+            <PosterSlider 
+            title = "Online Streaming Events"
+            subtitle = "Online Streaming Events"
+            posters= {onlineStreamEvents}
+            isDark = {false}
+            />
+        </div>
     </>
   )
 }
